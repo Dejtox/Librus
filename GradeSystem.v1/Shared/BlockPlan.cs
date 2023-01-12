883c8c8bd6bc53e7e6bffc0c4bb5aa3bf3343c2b
Marcin <marcinczar2000@gmail.com>
2023-01-12 21:06:01 +0100

    Plan

---
+++ GradeSystem.v1/Shared/BlockPlan.cs
@@ -0,0 +1,9 @@
+﻿    public class BlockPlan
+{
+    public string Name { get; set; }
+    public DateTime Starthour { get; set; }
+    public DateTime Endhour { get; set; }
+    public bool IsEmpty { get; set; }
+
+    public Enrollment? Lesson { get; set; }
+}

d48cb988c38b244e40203711236fd64287ed4fb5
Ax3l_FN <filip.nowak@lo5.wroclaw.pl>
2025-03-05 17:44:47 +0100

    Admin Page wstępny

--- GradeSystem.v1/Server/obj/Debug/net7.0/GradeSystem.v1.Server.AssemblyInfo.cs
+++ GradeSystem.v1/Server/obj/Debug/net7.0/GradeSystem.v1.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GradeSystem.v1.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8db2cae8920e1c3619a27e4d820d8ef6a2be56a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5461b1297c45aa1f888f289f049a561d9c309994")]
 [assembly: System.Reflection.AssemblyProductAttribute("GradeSystem.v1.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GradeSystem.v1.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

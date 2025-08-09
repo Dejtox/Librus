fe2a2caaaf30327f5934c5333c14a4854d3ed477
patryk.c159753@gmail.com <patryk.c159753@gmail.com>
2025-08-09 22:19:05 +0200

    Nowymerge

--- GradeSystem.v1/Client/obj/Debug/net7.0/GradeSystem.v1.Client.AssemblyInfo.cs
+++ GradeSystem.v1/Client/obj/Debug/net7.0/GradeSystem.v1.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GradeSystem.v1.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f05fd2b50ac5d9bcf0596a0338bddaa7cc2fe0f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee5000ccbf328fb71f617a99691cca949bbf2b74")]
 [assembly: System.Reflection.AssemblyProductAttribute("GradeSystem.v1.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GradeSystem.v1.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

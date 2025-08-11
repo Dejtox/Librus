28935d99cb113f64a44625d0676fa2f754be8765
patryk.c159753@gmail.com <patryk.c159753@gmail.com>
2025-08-11 20:48:27 +0200

    Wersja Do zaciagniecia poprawionewszystkomoznadodacDC

--- GradeSystem.v1/Shared/obj/Debug/net7.0/GradeSystem.v1.Shared.AssemblyInfo.cs
+++ GradeSystem.v1/Shared/obj/Debug/net7.0/GradeSystem.v1.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GradeSystem.v1.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+156b1416b6b66437e46add4ae930cfedc6edebb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faa9494258e790d6f7db0877b25b6fe785cf39fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("GradeSystem.v1.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GradeSystem.v1.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
